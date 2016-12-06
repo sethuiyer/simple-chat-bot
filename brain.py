3f162cd8ddd186edc7d6a766a9de4bfdaa1ef3fd
Sethu Iyer <sethuiyer95@gmail.com>
2016-12-06 15:22:31 +0530

    Second Commit
    Just added interactivity.

--- brain.py
+++ brain.py
@@ -1,10 +1,19 @@
 import aiml
 import os
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("question",help="Go ahead with your question!")
+args = parser.parse_args()
 kernel = aiml.Kernel()
+kernel.verbose(False)
+kernel.setBotPredicate("name", "Rebecca")
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "startup.xml", commands = "'LOAD AIML B")
+    kernel.saveBrain("bot_brain.brn")
 
-kernel.learn('startup.xml')
-kernel.respond("load aiml b")
-
-while True:
-    print kernel.respond(raw_input("Enter your message >> "))
+# kernel now ready for use
+print kernel.respond(args.question)
+kernel.saveBrain("bot_brain.brn")
